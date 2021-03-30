fe0558a6ad3b3ea7a0536877531674e14ed2f1b1
Alex <popaalexovidiu@gmail.com>
2021-03-30 18:43:55 +0300

    added wbt tests

---
+++ src/test/java/AssignmentServiceTest.java
@@ -0,0 +1,62 @@
+import domain.Tema;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+
+public class AssignmentServiceTest {
+
+    private Service service;
+
+    @Before
+    public void setUp() {
+        String filenameStudent = "src/test/resources/fisiere/Studenti.xml";
+        String filenameTema = "src/test/resources/fisiere/Teme.xml";
+        String filenameNota = "src/test/resources/fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        Path file = Paths.get("src/test/resources/fisiere/Studenti.xml");
+        Files.write(file, Collections.singletonList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox></inbox>"), StandardCharsets.UTF_8);
+        file = Paths.get("src/test/resources/fisiere/Teme.xml");
+        Files.write(file, Collections.singletonList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox></inbox>"), StandardCharsets.UTF_8);
+        file = Paths.get("src/test/resources/fisiere/Note.xml");
+        Files.write(file, Collections.singletonList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox></inbox>"), StandardCharsets.UTF_8);
+
+    }
+
+    @Test
+    public void tc_1_addAssignment_validAssignment(){
+        service.addTema(new Tema("123","abc",1,1));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void tc_2_addAssignment_invalidAssignment(){
+        service.addTema(new Tema("","",1,1));
+    }
+}
